fc4028e5b3e3f2842c3799100a485dd1b89f9bca
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-05-26 11:58:37 +0100

    feat(lottery-v2): Basic page, hero, state & buy modal setup

--- src/views/Pools/components/CakeVaultCard/VaultStakeModal.tsx
+++ src/views/Pools/components/CakeVaultCard/VaultStakeModal.tsx
@@ -80,6 +80,8 @@ const VaultStakeModal: React.FC<VaultStakeModalProps> = ({ pool, stakingMax, isR
     const sharesRemaining = userShares.minus(shareStakeToWithdraw.sharesAsBigNumber)
     const isWithdrawingAll = sharesRemaining.lte(triggerWithdrawAllThreshold)
 
+    console.log(account)
+
     if (isWithdrawingAll) {
       const tx = await cakeVaultContract.withdrawAll()
       setPendingTx(true)
