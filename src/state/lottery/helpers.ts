8f54a1049dcdf1b72ddffefc6485618e26cebd74
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-06-09 17:01:17 +0100

    feat(lottery-v2): Add user lottery history and public lottery history response to redux

--- src/state/lottery/helpers.ts
+++ src/state/lottery/helpers.ts
@@ -1,14 +1,13 @@
 import { request, gql } from 'graphql-request'
 import { GRAPH_API_LOTTERY } from 'config/constants/endpoints'
+import { UserLotteryHistory, PastLotteryRound } from 'state/types'
 
-export const getPastLotteries = async (): Promise<{
-  lotteries: any[]
-}> => {
+export const getPastLotteries = async (): Promise<PastLotteryRound[]> => {
   const response = await request(
     GRAPH_API_LOTTERY,
     gql`
       query getLotteries {
-        lotteries(first: 500) {
+        lotteries(first: 1000) {
           id
           totalUsers
           totalTickets
@@ -18,32 +17,51 @@ export const getPastLotteries = async (): Promise<{
           startTime
           endTime
           ticketPrice
+          firstTicket
+          lastTicket
         }
       }
     `,
   )
 
-  return response
+  const { lotteries } = response
+  return lotteries
 }
 
-export const getUserPastLotteries = async (
-  account: string,
-): Promise<{
-  users: any[]
-}> => {
+export const getUserPastLotteries = async (account: string): Promise<UserLotteryHistory> => {
   const response = await request(
     GRAPH_API_LOTTERY,
     gql`
       query getUserHistory($account: ID!) {
         user(id: $account) {
-          id
           totalTickets
           totalCake
+          participation {
+            lottery {
+              id
+            }
+            claimed
+            totalTickets
+          }
         }
       }
     `,
     { account: account.toLowerCase() },
   )
 
-  return response
+  const { user } = response
+
+  // TODO: req should just match desired format rather than reformating
+  const formattedUser = user && {
+    totalCake: user.totalCake,
+    totalTickets: user.totalTickets,
+    pastRounds: user.participation.map((round) => {
+      return {
+        lotteryId: round?.lottery?.id,
+        claimed: round?.claimed,
+        totalTickets: round?.totalTickets,
+      }
+    }),
+  }
+  return formattedUser
 }
