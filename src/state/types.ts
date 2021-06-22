360cbf74ca72cd6149b45d4dc69e7f1b7dffbc1b
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-06-22 11:32:04 +0100

    feat(lottery-v2): Improve round transitions, countdowns and date formatting

--- src/state/types.ts
+++ src/state/types.ts
@@ -285,6 +285,7 @@ export interface LotteryState {
   currentLotteryId: string
   maxNumberTicketsPerBuyOrClaim: string
   currentRound: LotteryRound
+  nextRound?: LotteryRound
   pastLotteries?: PastLotteryRound[]
   userLotteryHistory?: UserLotteryHistory
 }
