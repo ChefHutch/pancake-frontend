b023f0186884e984357a96ee7fc5315d2f8950c7
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-07-09 16:33:00 +0100

    feat(lottery-history): Round round swticher pieces added

--- src/views/Lottery/components/AllHistoryCard/RoundSwitcher.tsx
+++ src/views/Lottery/components/AllHistoryCard/RoundSwitcher.tsx
@@ -1,33 +1,62 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { Card, CardBody, Text, CardFooter, Flex, Heading } from '@pancakeswap/uikit'
+import { Card, IconButton, ArrowForwardIcon, ArrowBackIcon, Flex, Heading, Input } from '@pancakeswap/uikit'
 import { useTranslation } from 'contexts/Localization'
 import { useLottery } from 'state/hooks'
 
-const StyledCard = styled(Card)`
-  ${({ theme }) => theme.mediaQueries.xs} {
-    min-width: 320px;
-  }
+const StyledInput = styled(Input)`
+  width: 60px;
 `
 
-const RoundSwitcher = styled(CardBody)`
-  min-height: 240px;
+const StyledIconButton = styled(IconButton)<{ disabled: boolean }>`
+  ${({ disabled }) =>
+    disabled
+      ? `
+  background-color: red;
+  color: red;
+`
+      : ``}
 `
 
 interface RoundSwitcherProps {
+  isLoading: boolean
   activeRound: number
   mostRecentRound: number
-  setActiveRound: React.Dispatch<React.SetStateAction<number>>
+  handleInputChange: (event: any) => void
 }
 
-const YourHistoryCard: React.FC<RoundSwitcherProps> = ({ activeRound, mostRecentRound, setActiveRound }) => {
+const RoundSwitcher: React.FC<RoundSwitcherProps> = ({
+  isLoading,
+  activeRound,
+  mostRecentRound,
+  handleInputChange,
+}) => {
   const { t } = useTranslation()
 
   return (
-    <Flex>
-      <div> wat wat</div>
+    <Flex alignItems="center" justifyContent="space-between">
+      <Flex alignItems="center">
+        <Heading mr="8px">{t('Round')}</Heading>
+        <StyledInput
+          disabled={isLoading}
+          id="round-id"
+          name="round-id"
+          type="number"
+          value={activeRound}
+          scale="lg"
+          onChange={handleInputChange}
+        />
+      </Flex>
+      <Flex alignItems="center">
+        <StyledIconButton disabled={!activeRound} variant="text" scale="sm">
+          <ArrowBackIcon />
+        </StyledIconButton>
+        <StyledIconButton disabled={activeRound === mostRecentRound} variant="text" scale="sm">
+          <ArrowForwardIcon />
+        </StyledIconButton>
+      </Flex>
     </Flex>
   )
 }
 
-export default YourHistoryCard
+export default RoundSwitcher
--- src/views/Lottery/components/AllHistoryCard/index.tsx
+++ src/views/Lottery/components/AllHistoryCard/index.tsx
@@ -20,20 +20,32 @@ const YourHistoryCard = () => {
   const { t } = useTranslation()
   const {
     currentLotteryId,
-    currentRound: { status },
+    currentRound: { status, isLoading },
   } = useLottery()
   const currentLotteryIdAsInt = parseInt(currentLotteryId)
   const mostRecentFinishedRoundId =
     status === LotteryStatus.CLAIMABLE ? currentLotteryIdAsInt : currentLotteryIdAsInt - 1
   const [activeRound, setActiveRound] = useState(mostRecentFinishedRoundId)
 
+  const handleInputChange = (event) => {
+    const {
+      target: { value },
+    } = event
+    if (value) {
+      setActiveRound(parseInt(value, 10))
+    } else {
+      setActiveRound(null)
+    }
+  }
+
   return (
     <StyledCard>
       <StyledCardBody>
         <RoundSwitcher
+          isLoading={isLoading}
           activeRound={activeRound}
           mostRecentRound={mostRecentFinishedRoundId}
-          setActiveRound={setActiveRound}
+          handleInputChange={handleInputChange}
         />
       </StyledCardBody>
     </StyledCard>
