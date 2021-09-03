b7464699e44368ef9474a24f2ca68c6d8f75a013
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-09-03 10:45:24 +0100

    chore(tokens): Remove unnecessary get bnb/cake address helpers

--- src/hooks/useHasCakeBalance.ts
+++ src/hooks/useHasCakeBalance.ts
@@ -1,12 +1,12 @@
 import BigNumber from 'bignumber.js'
-import { getCakeAddress } from 'utils/addressHelpers'
+import tokens from 'config/constants/tokens'
 import useTokenBalance from './useTokenBalance'
 
 /**
  * A hook to check if a wallet's CAKE balance is at least the amount passed in
  */
 const useHasCakeBalance = (minimumBalance: BigNumber) => {
-  const { balance: cakeBalance } = useTokenBalance(getCakeAddress())
+  const { balance: cakeBalance } = useTokenBalance(tokens.cake.address)
   return cakeBalance.gte(minimumBalance)
 }
 
